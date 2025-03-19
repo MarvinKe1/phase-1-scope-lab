61f3e9087819fed1c0c8fdf0f6b9572c701a179c
Marvin Mango <marvinke2001@gmail.com>
2025-03-19 23:05:39 +0300

    Scope Lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+// Declare a variable in global scope called customerName using var
+var customerName = 'bob';
+
+// Write a function that accesses the global customerName and uppercases it
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Write a function that declares a global variable bestCustomer
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // This will implicitly declare bestCustomer in global scope
+}
+
+// Write a function that overwrites the global bestCustomer variable
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Declare a constant in global scope and try to change it
+const leastFavoriteCustomer = 'Alice';
+
+function changeLeastFavoriteCustomer() {
+  // This will throw an error because you cannot reassign a constant
+  leastFavoriteCustomer = 'Bob';
+}
